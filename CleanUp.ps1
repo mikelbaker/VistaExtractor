d879c4776bf02ebc00b8d978ec4daca4f3f3aea2
mikelbaker <michaelbakerassociates@outlook.com>
2023-04-30 16:57:54 -0400

    fixes for windos

--- CleanUp.ps1
+++ CleanUp.ps1
@@ -12,14 +12,17 @@ New-Item -ItemType Directory -Path bay/code -Force | Out-Null
 New-Item -ItemType Directory -Path bay/db/rou -Force | Out-Null
 New-Item -ItemType Directory -Path bay/jrn/jrn -Force | Out-Null
 New-Item -ItemType Directory -Path bay/jrn/alt -Force | Out-Null
+New-Item -ItemType Directory -Path bay/sys -Force | Out-Null
 New-Item -ItemType Directory -Path tam/code -Force | Out-Null
 New-Item -ItemType Directory -Path tam/db/rou -Force | Out-Null
 New-Item -ItemType Directory -Path tam/jrn/jrn -Force | Out-Null
 New-Item -ItemType Directory -Path tam/jrn/alt -Force | Out-Null
+New-Item -ItemType Directory -Path tam/sys -Force | Out-Null
 New-Item -ItemType Directory -Path wpb/code -Force | Out-Null
 New-Item -ItemType Directory -Path wpb/db/rou -Force | Out-Null
 New-Item -ItemType Directory -Path wpb/jrn/jrn -Force | Out-Null
 New-Item -ItemType Directory -Path wpb/jrn/alt -Force | Out-Null
+New-Item -ItemType Directory -Path wpb/sys -Force | Out-Null
 
 New-Item -ItemType Directory -Path v08/code -Force | Out-Null
 New-Item -ItemType Directory -Path v08/db/v08/bay/rou -Force | Out-Null
@@ -27,19 +30,21 @@ New-Item -ItemType Directory -Path v08/db/v08/tam/rou -Force | Out-Null
 New-Item -ItemType Directory -Path v08/db/v08/wpb/rou -Force | Out-Null
 New-Item -ItemType Directory -Path v08/jrn/jrn -Force | Out-Null
 New-Item -ItemType Directory -Path v08/jrn/alt -Force | Out-Null
+New-Item -ItemType Directory -Path v08/sys -Force | Out-Null
 
 New-Item -ItemType Directory -Path vx130/code -Force | Out-Null
 New-Item -ItemType Directory -Path vx130/db/vx130/vx130data -Force | Out-Null
 New-Item -ItemType Directory -Path vx130/db/vx130/vx130code -Force | Out-Null
 New-Item -ItemType Directory -Path vx130/jrn/jrn -Force | Out-Null
 New-Item -ItemType Directory -Path vx130/jrn/alt -Force | Out-Null
+New-Item -ItemType Directory -Path vx130/sys -Force | Out-Null
 
 $current_directory = (pwd).path
-docker run -v $current_directory\bay\code:/code -v $current_directory\bay\sys:/irissys -v $current_directory\bay\db:/irisdb -v $current_directory\bay\jrn:/irisjrn --name bay -d intersystems/iris:2022.1.2.574.0
-docker run -v $current_directory\tam\code:/code -v $current_directory\tam\sys:/irissys -v $current_directory\tam\db:/irisdb -v $current_directory\tam\jrn:/irisjrn --name tam -d intersystems/iris:2022.1.2.574.0
-docker run -v $current_directory\wpb\code:/code -v $current_directory\wpb\sys:/irissys -v $current_directory\wpb\db:/irisdb -v $current_directory\wpb\jrn:/irisjrn --name wpb -d intersystems/iris:2022.1.2.574.0
-docker run -v $current_directory\v08\code:/code -v $current_directory\v08\sys:/irissys -v $current_directory\v08\db:/irisdb -v $current_directory\v08\jrn:/irisjrn --name v08 -d intersystems/iris:2022.1.2.574.0
-docker run -v $current_directory\vx130\code:/code -v $current_directory\vx130\sys:/irissys -v $current_directory\vx130\db:/irisdb -v $current_directory\vx130\jrn:/irisjrn --name vx130 -d intersystems/iris:2022.1.2.574.0
+docker run -v $current_directory\bay\code:/code -v $current_directory\bay\sys:/irissys -v $current_directory\bay\db:/irisdb -v $current_directory\bay\jrn:/irisjrn --name bay -d intersystems/iris:2023.1.0.218.0
+docker run -v $current_directory\tam\code:/code -v $current_directory\tam\sys:/irissys -v $current_directory\tam\db:/irisdb -v $current_directory\tam\jrn:/irisjrn --name tam -d intersystems/iris:2023.1.0.218.0
+docker run -v $current_directory\wpb\code:/code -v $current_directory\wpb\sys:/irissys -v $current_directory\wpb\db:/irisdb -v $current_directory\wpb\jrn:/irisjrn --name wpb -d intersystems/iris:2023.1.0.218.0
+docker run -v $current_directory\v08\code:/code -v $current_directory\v08\sys:/irissys -v $current_directory\v08\db:/irisdb -v $current_directory\v08\jrn:/irisjrn --name v08 -d intersystems/iris:2023.1.0.218.0
+docker run -v $current_directory\vx130\code:/code -v $current_directory\vx130\sys:/irissys -v $current_directory\vx130\db:/irisdb -v $current_directory\vx130\jrn:/irisjrn --name vx130 -d intersystems/iris:2023.1.0.218.0
 sleep 20
 
 docker exec -it --user root bay chown -R irisowner:irisowner /irisdb /irisjrn /irissys /code
